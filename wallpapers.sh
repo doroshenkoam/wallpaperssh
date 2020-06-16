598cdf840389b93c452ee5d111dfc09c5eb4d054
Alexey Doroshenko <spartacity@mail.ru>
2020-06-16 17:29:02 +0300

    [v] version 2.0.0

--- wallpapers.sh
+++ wallpapers.sh
@@ -9,7 +9,20 @@ while :
 do
     for filej in $(cd $1 && find . -name '*.jpg' -type f); do
             sleep $2
-            echo "Выставляем изображение фоновым рисунком рабочего стола "$filej
-            gsettings set org.gnome.desktop.background picture-uri file:///$1/$filej
+            case $3 in
+                s)
+                    echo "Выставляем изображение фоном экрана блокировки $filej"
+                    gsettings set org.gnome.desktop.screensaver picture-uri file:///$1/$filej
+                ;;            
+                w)
+                    echo "Выставляем изображение фоновым рисунком рабочего стола $filej"
+                    gsettings set org.gnome.desktop.background picture-uri file:///$1/$filej
+                ;;
+                *)
+                    echo "Выставляем изображение фоновым рисунком рабочего стола и фоном экрана блокировки $filej"
+                    gsettings set org.gnome.desktop.background picture-uri file:///$1/$filej
+                    gsettings set org.gnome.desktop.screensaver picture-uri file:///$1/$filej
+                ;;
+            esac
     done
 done
